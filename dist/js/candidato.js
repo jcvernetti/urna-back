7441494a27b37ff822a2d8b05fcb701aa4b64a53
Welisson Hudson Fernandes Lima <welisson.lima@capgemini.com>
2021-09-20 17:42:17 -0300

    Criação da classe candidato em JS

---
+++ dist/js/candidato.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Candidato = void 0;
+var Candidato = /** @class */ (function () {
+    function Candidato(nome, numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+    Object.defineProperty(Candidato.prototype, "nome", {
+        get: function () {
+            return this._nome;
+        },
+        set: function (value) {
+            this.nome = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Candidato.prototype, "numero", {
+        get: function () {
+            return this._numero;
+        },
+        set: function (value) {
+            this.numero = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Candidato;
+}());
+exports.Candidato = Candidato;

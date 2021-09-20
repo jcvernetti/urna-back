0d59ad7be15a52bd392c2997428e82164127bbf5
Welisson Hudson Fernandes Lima <welisson.lima@capgemini.com>
2021-09-20 17:43:25 -0300

    Criação do EndPoint '/candidatos' com método POST

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,10 @@
+import { Candidato } from './candidato';
 import * as express from "express";
 
 const app: any = express();
 const porta: number = 8080;
+
+const candidatos: Array<Candidato> = [];
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -10,3 +13,9 @@ app.listen(porta, function(){
     console.log(`Servidor rodando na porta ${porta}.`);
 })
 
+app.post("/candidatos", function(req: any, resp: any): void{
+    let candidato = req.body.candidato;
+    candidatos.push(candidato);
+
+    resp.json({mensagem: "Candidato salvo com sucesso !", status: 200});
+})
--- dist/js/app.js
+++ dist/js/app.js
@@ -3,8 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
 var porta = 8080;
+var candidatos = [];
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.listen(porta, function () {
     console.log("Servidor rodando na porta " + porta + ".");
+});
+app.post("/candidatos", function (req, resp) {
+    var candidato = req.body.candidato;
+    candidatos.push(candidato);
+    resp.json({ mensagem: "Candidato salvo com sucesso !", status: 200 });
 });

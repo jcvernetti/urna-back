5b95d932e81c9db0e815fba644044e3279553603
João Carlos Vernetti dos Santos <joao.a.santos@capgemini.com>
2021-09-20 22:37:50 -0300

    add login sem JTW

--- app/app.js
+++ app/app.js
@@ -1,9 +1,10 @@
-//import * as express from "express";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 var porta = 8080;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.listen(porta, function () {
     console.log("Servidor rodando na porta " + porta + ".");
 });
--- app/app.ts
+++ app/app.ts
@@ -1,12 +1,24 @@
-import * as express from "express";
+//import * as express from "express";
+const express = require("express")
 
-const app: any = express();
-const porta: number = 8080;
+const app = express();
 
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 
+
+const porta: number = 8080;
 app.listen(porta, function(){
     console.log(`Servidor rodando na porta ${porta}.`);
 })
 
+app.post("/login", function (req, resp) {
+    
+    if (req.body.user == "admUrna" && req.body.pass === 1010) {
+
+        return resp.json({usuario: "admUrna", autorizado: true})
+    }
+    resp.status(401).end()
+
+    //nmp i jsonwebtoken
+})
